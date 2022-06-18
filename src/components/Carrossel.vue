1cc659643ec347588d2ec32f50c84a2402c2c087
FOXXX <75998678+FOXXX2K@users.noreply.github.com>
2022-06-18 03:36:35 -0300

    Ajustes em bugs do carrossel

--- src/components/Carrossel.vue
+++ src/components/Carrossel.vue
@@ -4,10 +4,10 @@
       <p>O maior desafio de ser freelancer é que você tem que ser um pouquinho de tudo ( Designer, Programador, etc), Este projeto foi desafiador, pois o cliente me passou apenas as cores e as fontes que ele gostaria de ter no site dele, Mas como sou apaixonado por desafios fui fazendo detalhe por detalhe até chegar neste resultado, E o mais importante é que o cliente ficou bem satisfeito com o resultado. 😍</p>
     </div>
 
+    <div class="conteudo">
       <div class="arrow-left" @click="arrowLeft">
         <img src="@/assets/arrowleft.svg" alt="">
       </div>
-    <div class="conteudo">
       <div class="projeto1">
         <video class="video" width="800px" style="border: 2px solid green; border-radius: 10px" autoplay muted loop>
           <source  src="@/assets/projects/ProjetoStabelPiercer.mp4" type="video/mp4">  
@@ -22,10 +22,10 @@
           <source  src="@/assets/projects/ProjetoStabelPiercer.mp4" type="video/mp4">  
         </video>
       </div>
-    </div>
-      <div class="arrow-right">
+      <div class="arrow-right" @click="arrowRight">
         <img src="@/assets/arrowright.svg" alt="">
       </div>
+    </div>
     <div style="color: white; display: flex; margin-left: 20px" class="stacks">
       <p>JS</p>
       <p>VUE</p>
@@ -45,9 +45,20 @@ export default {
   },
   methods: {
     arrowLeft() {
+      this.images--
+      let element = document.querySelectorAll('.projeto1 video')
+        console.log(this.countPixels)
+      
+      if (this.images <= 0) {
+        this.images = 1
+        this.countPixels -= 800
+      }
+      element.forEach(el => {
+        el.style.transform = `translateX(-${this.countPixels}px)`
+      });
+    },
+    arrowRight() {
       this.images++
-      console.log( this.images)
-      console.log( this.countPixels)
       let element = document.querySelectorAll('.projeto1 video')
       this.countPixels += 800
       if (this.images > element.length -1) {
@@ -74,19 +85,20 @@ export default {
   }
   .conteudo {
     
-    width: 800px;
     position: relative;
     z-index: 30;
     display: flex;
     align-items: center;
-      overflow: hidden;
+
     .projeto1 {
       
+      width: 800px;
       display: flex;
+        overflow: hidden;
       .video {
+        width: 100%;
         transition: transform 0.5s;
         transform: translateX(0);
-        overflow: hidden;
         object-fit: cover;
       }
       .stacks-sociais {
